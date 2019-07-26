9ae9f15dd763d79478d9f1286f4e61a225ef340c
Abdulqadir <qadirabdul20@yahoo.com>
2019-07-26 18:37:09 +0200

    Navigation Bar without menu button

---
+++ app/src/main/java/app/easylink/navigationdrawer/MainActivity.java
@@ -0,0 +1,22 @@
+package app.easylink.navigationdrawer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
