a72ab0d681e60fd738c929a762ecc04add07ea73
Abdulqadir <qadirabdul20@yahoo.com>
2019-08-04 04:30:28 +0200

    *Menu Button and Background is added for the Navigationbar *Image is added to the drawable * Header.xml is added

--- app/src/main/java/app/easylink/navigationdrawer/MainActivity.java
+++ app/src/main/java/app/easylink/navigationdrawer/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is the first part
+        dl=(DrawerLayout)findViewById(R.id.drawer);
+        t=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        dl.addDrawerListener(t);//this is the first part
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(t.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
