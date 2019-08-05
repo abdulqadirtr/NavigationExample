7cf110d1a35f091b4d5c2992585353b4dc1b3f4b
Abdulqadir <qadirabdul20@yahoo.com>
2019-08-05 05:31:34 +0200

    * On Navigation Item select are added * Buttons selections are working

--- app/src/main/java/app/easylink/navigationdrawer/MainActivity.java
+++ app/src/main/java/app/easylink/navigationdrawer/MainActivity.java
@@ -1,12 +1,15 @@
 package app.easylink.navigationdrawer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
@@ -19,19 +22,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dl=(DrawerLayout)findViewById(R.id.drawer);
-        t=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        dl = (DrawerLayout) findViewById(R.id.drawer);
+        t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
         dl.addDrawerListener(t);//this is the first part
         t.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       final NavigationView navigationView=(NavigationView)findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.events) {
+                    Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(t.onOptionsItemSelected(item)){
+        if (t.onOptionsItemSelected(item)) {
             return true;
         }
+        switch (item.getItemId()) {
+            case R.id.events:
+               // Toast.makeText(this, "Hello Welcome This is Events", Toast.LENGTH_LONG).show();
+
+                break;
+
+
+        }
         return super.onOptionsItemSelected(item);
+
     }
+
+
 }
